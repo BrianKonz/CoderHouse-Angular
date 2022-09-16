13caad7dd661744791041c5c3a3f3cb465ba4cf0
Brian.Konz <brianekonz@gmail.com>
2022-09-15 22:52:58 -0300

    primer entregable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentsComponent } from './components/students/students.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ToolbarComponent,
+    StudentsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
