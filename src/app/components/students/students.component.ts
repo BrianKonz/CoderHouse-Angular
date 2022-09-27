14286c466450324f44f1d00b5a2865a62b700a93
Brian.Konz <brianekonz@gmail.com>
2022-09-26 21:09:12 -0300

    Directivas

--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/alumnos';
 
 @Component({
   selector: 'app-students',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+
+  listaAlumnos:Array<Persona> = [
+    
+    {nombre: 'Nicolas Cage', edad: 44},
+    {nombre: 'Demi Moore', edad: 32},
+    {nombre: 'Ricky Martin', edad: 51},
+    {nombre: 'Bruce Willis', edad: 33},
+    {nombre: 'Elon Musk', edad: 61},
+    
+  ]
+
+
+  variable1: Number = 5;
 
   constructor() { }
 
